2843610a2189d00c6d90ce34206e05d33b85690b
Enio N. de Souza <enionsouza@gmail.com>
2021-10-27 11:58:34 -0400

    feat: Find a way to decrease the execution time of the first query.

--- schema.sql
+++ schema.sql
@@ -52,3 +52,9 @@ INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id F
 
 -- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- Use EXPLAIN ANALYZE on QUERY#1 (SELECT COUNT(*) FROM visits where animal_id = 4)
+-- vetclinic=# explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+
+-- Find a way to decrease the execution time of the first query.
+CREATE INDEX animal_index ON visits(animal_id);
